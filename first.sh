4fa5ae7476d8844f9c24488b736a6d4eea73feca
Sai-devops-22 <badesaivenkat@gmail.com>
2025-05-17 21:08:22 +0530

    first added

--- first.sh
+++ first.sh
@@ -4,7 +4,8 @@ USER=$(id -u)
 
 if [ $USER -ne 0 ]
 then
-    echo "ERR0R! Please enter into roo user"
+    echo "ERR0R! Please enter into root user"
+    exit 1
 else
     echo "you are in root user"
 fi
