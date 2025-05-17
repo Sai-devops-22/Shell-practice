b5b2dc952335fdfaabc8942dd2af6605cebc202a
Sai-devops-22 <badesaivenkat@gmail.com>
2025-05-17 19:49:17 +0530

    first added

--- first.sh
+++ first.sh
@@ -2,7 +2,10 @@
 
 NUMBER1=$1
 NUMBER2=$2
-TIMESTAMP=$(date)
-echo "The Timestamp is:$TIMESTAMP"
-SUM=$((NUMBER1+NUMBER2))
-echo "THE SUM OF TWO IS:$SUM"
+# TIMESTAMP=$(date)
+# echo "The Timestamp is:$TIMESTAMP"
+# echo "the"
+# SUM=$((NUMBER1+NUMBER2))
+# echo "THE SUM OF TWO IS:$SUM"
+echo "hi $PERSON1"
+echo "HI $PERSON2"
