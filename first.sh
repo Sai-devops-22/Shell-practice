6c60282b1cbeccdfcbf4e2792b8701921323a7bd
Sai-devops-22 <badesaivenkat@gmail.com>
2025-05-17 19:54:28 +0530

    first added

--- first.sh
+++ first.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-PERSON1=$1
-PERSON2=$2
-# TIMESTAMP=$(date)
-# echo "The Timestamp is:$TIMESTAMP"
-# echo "the"
-# SUM=$((NUMBER1+NUMBER2))
-# echo "THE SUM OF TWO IS:$SUM"
-echo "hi $NUMBER1"
-echo "HI $NUMBER2"
+MOVUES=("KGF" "KGF2" "SALAAR")
+echo "the $(MOVIES)[0]"
