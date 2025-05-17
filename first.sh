c4def22ed24643d25e0472e2ecb77da7f7b8ee5f
Sai-devops-22 <badesaivenkat@gmail.com>
2025-05-17 20:40:40 +0530

    first added

--- first.sh
+++ first.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-echo "$@"
-echo "$#"
-echo "$0"
-echo "$PWD"
-echo "$HOME"
-echo "$USER"
-echo "$$"
-echo "$!"
+NUMBER1=$1
+if ($NUMBER1 -ln 10)
+then 
+    echo "Then $NUMBER1 is less then 10"
+else
+    echo "The $NUMBER1 is greater than 10"
+fi
 
