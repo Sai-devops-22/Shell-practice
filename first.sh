dc220ad620e4779bdc993f9352e337d050b05f77
Sai-devops-22 <badesaivenkat@gmail.com>
2025-05-17 20:42:14 +0530

    first added

--- first.sh
+++ first.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 NUMBER1=$1
-if ($NUMBER1 -ln 10)
+if ( $NUMBER1 -ln 10 )
 then 
-    echo "Then $NUMBER1 is less then 10"
+    echo "The $NUMBER1 is less then 10"
 else
     echo "The $NUMBER1 is greater than 10"
 fi
