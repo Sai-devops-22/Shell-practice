941c93641faa08efb8116bdc7e9c8154c78f75ab
Sai-devops-22 <badesaivenkat@gmail.com>
2025-05-17 20:31:37 +0530

    first added

--- first.sh
+++ first.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo "All variable passed to script:$@"
-echo "SCRIPTS:$#"
-echo "The script name: $0"
-echo "The pwd is:$PWD"
-echo "The home directory:$HOME"
-echo "The user:$USER"
-echo "The PID:$$"
-echo "The PID:$!"
+echo "$@"
+echo "$#"
+echo "$0"
+echo "$PWD"
+echo "$HOME"
+echo "$USER"
+echo "$$"
+echo "$!"
 
