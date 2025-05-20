32d85a4fa7990e28d5394d743187f10e6ac29fa9
Sai-devops-22 <badesaivenkat@gmail.com>
2025-05-20 09:43:54 +0530

    shell

--- if-else.sh
+++ if-else.sh
@@ -2,9 +2,9 @@
 
 NUMBER1=$1
 
-if [ $1 -lt 10 ]
+if [ $NUMBER1 -lt 10 ]
 then 
-    echo "the $1 is less than 10"
+    echo "the $NUMBER1 is less than 10"
 else
-    echo "the $1 is greater than 10"
+    echo "the $NUMBER1 is greater than 10"
 fi
