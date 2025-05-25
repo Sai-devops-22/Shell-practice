a6a788fd8fff5d347d228f93452a4d6c64a3f70c
Sai-devops-22 <badesaivenkat@gmail.com>
2025-05-25 13:45:54 +0530

    msg

--- delete.sh
+++ delete.sh
@@ -1,12 +1,15 @@
 # #!/bin/bash
 
-# FILE='/home/ec2-user/app-logs'
-# #DELETE=$(find $FILE -name "*.log" -mtime +14 -delete)
-
-# DELETE=$(find "$FILE" -type f -name "*.log" -mtime +14 -print -delete)
-# echo $DELETE
+#PRINT THE OU[UT WITH PATH
+FILE='/home/ec2-user/app-logs'
 
 
+DELETE=$(find "$FILE" -type f -name "*.log" -mtime +14 -print -delete)
+echo $DELETE
+
+#PRINT THE OUTPUT ONT THE FILE NAME
 FILE_PATH='/home/ec2-user/app-logs'
 DELETE=$(find "$FILE_PATH" -type f -name "*.log" -mtime +14 -exec basename {} \; -delete) 
 echo $DELETE
+
+#DELETE=$(find $FILE -name "*.log" -mtime +14 -delete)
