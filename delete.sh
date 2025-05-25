da9af2a30e81f7f201cc75822533709ea1837e70
Sai-devops-22 <badesaivenkat@gmail.com>
2025-05-25 12:28:53 +0530

    msg

--- delete.sh
+++ delete.sh
@@ -3,5 +3,5 @@
 FILE='/home/ec2-user/app-logs'
 #DELETE=$(find $FILE -name "*.log" -mtime +14 -delete)
 
-DELETE=$(find "$FILE" -type f -name "*.log" -mtime +14 -print -delete)
+DELETE=$(find "$FILE" -type f -name "*.log" -mtime +14 -exec basename {} \; -delete)
 echo $DELETE
