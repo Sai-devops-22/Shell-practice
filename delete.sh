e8fda147a6e1fbfefffa34ad5302059cff8a2ceb
Sai-devops-22 <badesaivenkat@gmail.com>
2025-05-25 13:41:59 +0530

    msg

--- delete.sh
+++ delete.sh
@@ -8,4 +8,4 @@
 
 
 FILE_PATH='/home/ec2-user/app-logs'
-DELETE=$(find "$FILE" -type f -name "*.log" -mtime +14 -exec basename {} \; -delete)
+DELETE=$(find "$FILE_PATH" -type f -name "*.log" -mtime +14 -exec basename {} \; -delete)
