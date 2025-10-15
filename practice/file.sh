421bacf0257f9c3604ba2d1ee9064467869e7167
Sai-devops-22 <badesaivenkat@gmail.com>
2025-10-15 08:04:23 +0530

    added_new_file

--- practice/file.sh
+++ practice/file.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-Person1=pradeep
-Person2=ravi
+# Person1=pradeep
+# Person2=ravi
 
-echo "$Person1:: hey hi $Person2"
-echo "$Person2:: hi $Person1"
+# echo "$Person1:: hey hi $Person2"
+# echo "$Person2:: hi $Person1"
 
-values=("one", "two", "three")
-echo "values are ${values[0]}"
+# values=("one"  "two", "three")
+# echo "values are ${values[0]}"
+
+NUMBER1=$1
+NUMBER2=$2
+TIMESTAMP=${date}
+SUM=$((NUMBER1+NUMBER2))
+echo "the sum of two is $SUM
