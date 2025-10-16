2f2a3e216d12e1e512a979bef275385653b1ea39
Sai-devops-22 <badesaivenkat@gmail.com>
2025-10-16 06:54:45 +0530

    added

--- practice/file.sh
+++ practice/file.sh
@@ -9,10 +9,24 @@
 # values=("one"  "two", "three")
 # echo "values are ${values[0]}"
 
-NUMBER1=$1
-NUMBER2=$2
-TIMESTAMP=$(date)
-echo ${TIMESTAMP}
-SUM=$((NUMBER1+NUMBER2))
-echo "the sum of two is $SUM"
+# NUMBER1=$1
+# NUMBER2=$2
+# TIMESTAMP=$(date)
+# echo ${TIMESTAMP}
+# SUM=$((NUMBER1+NUMBER2))
+# echo "the sum of two is $SUM"
 
+
+USER=$(id -u)
+TIMESTAMP=$(date)
+
+if [ $USER -eq 0 ]
+then
+    echo "the script executed at $TIMESTAMP"
+    echo "you are a root user"
+
+else
+    echo "the script executed at $TIMESTAMP"
+    echo "you are not a root user"
+    exit 1
+fi
