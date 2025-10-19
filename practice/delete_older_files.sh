ab778c3ad848ba36bf55d9b4422093da1f0e94d3
Sai-devops-22 <badesaivenkat@gmail.com>
2025-10-19 20:23:42 +0530

    added

--- practice/delete_older_files.sh
+++ practice/delete_older_files.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-a=0
+# a=0
 
-while [ $a -lt 10 ];
+# while [ $a -lt 10 ];
+# do
+#     echo $a
+#     ((a++))
+# done
+
+SOURCE_DIR=/home/ec2-user/apps
+file_to_delete=$(find $SOURCE_DIR -name "*.log" -mtime +14)
+
+while IFS= read -r filepath 
 do
-    echo $a
-    ((a++))
-done
+    rm -rf $filepath
+done <<< $file_to_delete
