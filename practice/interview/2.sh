bfd65f49775e647819dfbe965bca1e085e13d660
Sai-devops-22 <badesaivenkat@gmail.com>
2026-06-09 07:16:43 +0530

    modifiedfiles

--- practice/interview/2.sh
+++ practice/interview/2.sh
@@ -3,6 +3,15 @@
 num1=10
 num2=20
 
+USER_ID=$(id -u)
+
+if [ $USER_ID -ne 0]
+then
+    echo "run with root user"
+else
+    echo "you are root user"
+fi
+
 if [ $num1 -gt $num2]
 then
     echo "the $num1 is bigger"
